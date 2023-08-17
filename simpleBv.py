d6a12b60932780062ffcee4327c9ebe217970831
Robbie Shaw <robbie.shaw@stfc.ac.uk>
2023-08-17 13:21:37 +0100

    Very Basic Functionality

--- simpleBv.py
+++ simpleBv.py
@@ -1,4 +1,5 @@
 import pymatgen.core as pmg
+import math
 
 def generate_structure(cifFile:str) -> pmg.Structure:
     return pmg.Structure.from_file(cifFile)
@@ -46,7 +47,65 @@ def find_neigbours(eCell:pmg.Structure, grid:[(int,int,int)], radius:float):
 
     return result
 
+# def find_distance(site1, site2):
+#     deltaX = site2[0] - site1[0]
+#     deltaY = site2[1] - site1[1]
+#     deltaZ = site2[2] - site1[2]
 
+#     return math.sqrt(deltaX^2 + deltaY^2 + deltaZ^2)
+
+def find_distances_to_neigbours(eCell:pmg.Structure, grid:[(int,int,int)], radius:float):
+    result = {}
+
+    for pixel in grid:
+        neighbours = eCell.get_sites_in_sphere(pixel, radius)
+        pixelList = []
+        for neighbour in neighbours:
+            pixelList.append((neighbour.species_string, neighbour.distance_from_point(pixel)))
+        result[pixel] = pixelList
+
+    return result
+
+def bv_equation(r0, ri, b):
+    return math.exp((r0-ri)/b)
+
+def calculate_bv(distanceList:dict, bvParams):
+
+    result = {}
+    
+    for pixel, data in distanceList.items():
+        
+        bvSum = 0
+
+        for site, distance in data:
+            bvSum += bv_equation(bvParams[site][0], distance, bvParams[site][1])
+
+        result[pixel] = bvSum
+
+    return result
+        
+
+def export_grd(fileName, result, res, structure:pmg.Structure):
+    with open(fileName, 'w') as file:
+        file.write("%s\n" % (structure.formula))
+        file.write("%f %f %f %f %f %f\n" % structure.lattice.parameters)
+        file.write("%i %i %i\n" % (res, res, res))
+
+        lnProgress = 0
+        for value in result.values():
+            file.write("    %.10e" % (value))
+            lnProgress += 1
+            if lnProgress == 6:
+                file.write("\n")
+                lnProgress = 0
+
+
+resolution = 20
+bvParams = {'Sn2+':(1.925, 0.37), 'Pb2+':(2.03, 0.37)}
 structure = generate_structure("cif-files/Ternary Fluorides/EntryWithCollCode152949 (PbSnF4).cif")
-eCell, grid = create_centred_grid(structure, 5, 5)
-print(find_neigbours(eCell, grid, 5))
+structure.remove_species(["F-"])
+print(structure)
+eCell, grid = create_centred_grid(structure, 5, resolution)
+distanceList = find_distances_to_neigbours(eCell, grid, 6)
+result = calculate_bv(distanceList, bvParams)
+export_grd("PbSnF2.grd", result, resolution, structure)
